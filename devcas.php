ec7d734de3ea6d45016ee2b673a51cf306714eff
RobbeCastaar <robbe@castaar.com>
2025-07-25 12:14:55 +0200

    edit name

--- devcas.php
+++ devcas.php
@@ -1,11 +1,10 @@
-
 <?php
 /*
     Plugin Name: DevCas
     Plugin URI: https://github.com/NeZotVanCastaar/devcas
     Description: Een verzameling handige SEO-tools, bulk editors en custom functionaliteit.
     Version: 1.0.0
-    Author: Alec Meganck & Robbe Cooman
+    Author: Castaar – Alec Meganck & Robbe Cooman
     Author URI: https://castaar.com
 */
 
@@ -16,20 +15,17 @@ require plugin_dir_path(__FILE__) . 'plugin-update-checker/plugin-update-checker
 
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
-// 2. Stel de update checker in
+// 2. Stel de update checker in (zonder token!)
 $updateChecker = PucFactory::buildUpdateChecker(
     'https://github.com/NeZotVanCastaar/devcas/',
     __FILE__,
     'devcas'
 );
 
-// Optioneel: Branch instellen (meestal main)
+// 3. Branch instellen (meestal 'main')
 $updateChecker->setBranch('main');
 
-// Als het een privé GitHub repo is, voeg token toe:
-// $updateChecker->setAuthentication('YOUR_PERSONAL_ACCESS_TOKEN');
-
-// 3. Laad alle submodules uit de /modules map
+// 4. Submodules laden
 foreach (glob(plugin_dir_path(__FILE__) . 'modules/*.php') as $module) {
     include_once $module;
 }
