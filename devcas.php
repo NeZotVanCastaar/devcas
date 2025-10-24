5ca2c740fb9ef920e1bf51fd1672413606ca6373
RobbeCastaar <robbe@castaar.com>
2025-10-24 13:33:02 +0200

    Update version

--- devcas.php
+++ devcas.php
@@ -3,7 +3,7 @@
     Plugin Name: DevCas
     Plugin URI: https://github.com/NeZotVanCastaar/devcas
     Description: Een verzameling handige SEO-tools, bulk editors en custom functionaliteit.
-    Version: 1.1.7
+    Version: 1.0.7
     Author: Castaar – Alec Meganck & Robbe Cooman
     Author URI: https://castaar.com
 */
